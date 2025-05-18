cf0ca0321f68b9c71971413944c69f81e61354ed
beafn28 <beafn23@gmail.com>
2025-05-18 18:49:43 +0200

    Fix rutas SPA con _redirects

--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   server: {
     proxy: {
-      '/api/ocr': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/ocultar': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/extraer': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/historial': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/rgb_split': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/ela': {
-        target: 'https://stegatoolkit-backend.onrender.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      },
-      '/api/analisis_forense': {
-        target: 'https://stegatoolkit-backend.onrender.com',
+      '/api': {
+        target: 'https://backend-falling-breeze-4140.fly.dev',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
