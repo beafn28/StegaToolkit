62157fc3a220821df77fa924ff23b23e58be9221
beafn28 <beafn23@gmail.com>
2025-05-18 20:02:35 +0200

    Fix CORS to allow frontend in Render

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://stegatoolkit.onrender.com"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
